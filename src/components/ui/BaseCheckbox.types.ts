bb0fb3b7efbf0c6b7643dcb3389e5905663f14eb
Shevja <vlsdki@gmail.com>
2026-01-20 21:49:16 +0700

    Обновлен Readme; Убран дублирующий тип label из checkbox props, который есть в BaseFieldProps;

--- src/components/ui/BaseCheckbox.types.ts
+++ src/components/ui/BaseCheckbox.types.ts
@@ -1,10 +1,9 @@
 import type {InputHTMLAttributes} from "vue";
-import type {BaseFieldProps} from "../../types/shared.ts";
+import type {BaseFieldProps} from "@/types/shared.ts";
 
 export type CheckboxValue = boolean | null
 
 export interface CheckboxProps extends BaseFieldProps {
     type: 'checkbox'
-    label?: string
     htmlAttrs?: InputHTMLAttributes & Record<string, unknown>
 }
